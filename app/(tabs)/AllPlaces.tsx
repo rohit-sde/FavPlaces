8383463fe8ded1fffc5c954e755ad693ba4b5d7b
Rohit Singh <rohitsinghpb4@gmail.com>
2026-02-26 13:53:18 +0530

    screens added

---
+++ app/(tabs)/AllPlaces.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function AllPlaces() {
+  return (
+    <View>
+      <Text>AllPlaces</Text>
+    </View>
+  );
+}
---
+++ app/components/PlacesList/PlacesList.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function PlacesList() {
+  return (
+    <View>
+      <Text>PlacesList</Text>
+    </View>
+  );
+}
