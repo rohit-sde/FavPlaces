58e51b20f4fd3782486a6042e6b2255e61ed9db0
Rohit Singh <rohitsinghpb4@gmail.com>
2026-02-27 18:25:37 +0530

    output added place data on all places screen

--- app/(tabs)/AllPlaces.tsx
+++ app/(tabs)/AllPlaces.tsx
@@ -1,5 +1,18 @@
+import { Place } from "@/models/place";
+import { useIsFocused } from "@react-navigation/native";
+import { useEffect, useState } from "react";
 import PlacesList from "../components/Places/PlacesList";
 
-export default function AllPlaces() {
-  return <PlacesList places={[]} />;
+export default function AllPlaces({ route }: { route: any }) {
+  const [loadedPlaces, setLoadedPlaces] = useState<Place[]>([]);
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (isFocused && route.params) {
+      const place = route.params.place;
+      setLoadedPlaces((curPlaces) => [...curPlaces, place]);
+    }
+  }, [isFocused, route]);
+
+  return <PlacesList places={loadedPlaces} />;
 }
